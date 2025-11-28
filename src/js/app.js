a6a833b3c52edb080e62e0f82bed6b8a75f766b6
MBello21 <magarbello@gmail.com>
2025-11-28 18:13:17 +0000

    Ejercicio con modo oscuro

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { name } from "file-loader";
 import "../style/index.css";
 
 /**
@@ -20,27 +21,65 @@ import "../style/index.css";
         role: null,
         country: null,
         city: null
-    }
- */
+    }*/
+
+const toggle = document.getElementById("container-toggle");
+toggle.onclick = () => {
+  const html = document.documentElement;
+  const activeTheme = html.getAttribute("data-theme");
+  activeTheme === "light"
+    ? (html.setAttribute("data-theme", "dark"),
+      localStorage.setItem("theme", "dark"))
+    : (html.setAttribute("data-theme", "light"),
+      localStorage.setItem("theme", "light"));
+};
+// Obtener elementos
+const savedTheme = localStorage.getItem("theme") || "light";
+document.documentElement.setAttribute("data-theme", savedTheme);
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let name = `${variables.name}`;
+  variables.name == null ? (name = `Your name`) : (name = `${variables.name}`);
+  let lastName = `${variables.lastName}`;
+  variables.lastName == null
+    ? (lastName = `Your lastName`)
+    : (lastName = `${variables.lastName}`);
+  let role = variables.role;
+  variables.role == null ? (role = "Web Developer") : (role = variables.role);
+  let city = variables.city;
+  variables.city == null ? (city = "Maimi") : (city = variables.city);
+  let country = variables.country;
+  variables.country == null ? (country = "USA") : (country = variables.country);
+  let twitter = variables.twitter;
+  variables.twitter == null ? (twitter = "") : (twitter = variables.twitter);
+  let github = variables.github;
+  variables.github == null ? (github = "") : (github = variables.github);
+  let linkedin = variables.linkedin;
+  variables.linkedin == null
+    ? (linkedin = "")
+    : (linkedin = variables.linkedin);
+  let instagram = variables.instagram;
+  variables.instagram == null
+    ? (instagram = "")
+    : (instagram = variables.instagram);
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://x.com/${twitter}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -58,7 +97,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (position-left or position-right)
-    socialMediaPosition: "position-right",
+    socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
     github: null,
